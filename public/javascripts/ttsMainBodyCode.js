ed164e23cc499f427f2f27d83b1fe927256a2d9f
Tyler Schneider <archerzen8@tamu.edu>
2022-04-16 18:31:01 -0500

    TTS implemented:
    Issue with refreshing page (must hit stop first)
    
    clicking the pause button first (highlights it and doesnt deselect

--- public/javascripts/ttsMainBodyCode.js
+++ public/javascripts/ttsMainBodyCode.js
@@ -5,6 +5,8 @@ onload = function () {
         //tts is supported
         var synth = speechSynthesis;
         var flag = false;
+        var pauseflag = false;
+        var stopflag = false;
 
         /* references to the buttons */
         var playEle = document.querySelector('#play');
@@ -15,6 +17,10 @@ onload = function () {
         playEle.addEventListener('click', onClickPlay);
         pauseEle.addEventListener('click', onClickPause);
         stopEle.addEventListener('click', onClickStop);
+
+        playEle.className = '';
+        stopEle.className = '';
+        pauseEle.className = '';
 
         function onClickPlay() {
             if(!flag){
@@ -29,25 +35,50 @@ onload = function () {
                 //document done being read
                 utterance.onend = function(){
                     flag = false;
+                    playEle.className = pauseEle.className = '';
+                    stopEle.className = 'stopped';
                 };
                 //starts narration
+                playEle.className = 'played';
+                stopEle.className = '';
                 synth.speak(utterance);
             }
             //if speech was paused
             if(synth.paused) { //unpause/resume narration
+                playEle.className = 'played';
+                stopEle.className = '';
+                pauseEle.className = '';
                 synth.resume();
             }
         }
 
         function onClickPause() {
-
+            if (synth.speaking && !synth.paused) { /* pause narration */
+                pauseEle.className = 'paused';
+                playEle.className = '';
+                synth.pause();
+            }
         }
 
         function onClickStop() {
+            if (synth.speaking) { /* stop narration */
+                stopEle.className = 'stopped';
+                playEle.className = pauseEle.className = '';
+                /* for safari */
+                flag = false;
 
+                synth.cancel();
+            }
         }
     }
     else {
         //not supported
+        msg = document.createElement('h5');
+        msg.textContent = "Detected no support for Speech Synthesis";
+        msg.style.textAlign = 'center';
+        msg.style.backgroundColor = 'red';
+        msg.style.color = 'white';
+        msg.style.marginTop = msg.style.marginBottom = 0;
+        document.body.insertBefore(msg, document.querySelector('div'));
     }
 }
